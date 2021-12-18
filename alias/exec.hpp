593913886a3e1be161916e4a903f563f24e2a51a
radj307 <radj307@gmail.com>
2021-12-18 14:55:25 -0500

    bump 307lib version, linux is needlessly supported

--- alias/exec.hpp
+++ alias/exec.hpp
@@ -1,8 +1,17 @@
 #pragma once
+#include <sysarch.h>
 #include <make_exception.hpp>
 
 #include <iostream>
 #include <array>
+
+#ifdef OS_WIN
+#define POPEN _popen
+#define PCLOSE _pclose
+#else
+#define POPEN popen
+#define PCLOSE pclose
+#endif
 
 namespace alias {
 	struct Exec {
@@ -12,10 +21,10 @@ namespace alias {
 
 		friend std::ostream& operator<<(std::ostream& os, const Exec& obj)
 		{
-			auto pipe(_popen(obj.command.c_str(), "r"));
+			auto pipe(POPEN(obj.command.c_str(), "r"));
 			if (!pipe) throw make_exception("Exec failed to open the pipe!");
 			for (std::array<char, 128> buffer{}; fread(&buffer, 1, buffer.size() - 1, pipe) != 0u; os << buffer.data()) {}
-			if (*(Global.return_code = std::make_unique<int>(_pclose(pipe))).get() == -1) throw make_exception("Exec failed to close the process!");
+			if (*(Global.return_code = std::make_unique<int>(PCLOSE(pipe))).get() == -1) throw make_exception("Exec failed to close the process!");
 			return os;
 		}
 	};
@@ -28,10 +37,10 @@ namespace alias {
 	 */
 	inline int exec(const char* command) noexcept(false)
 	{
-		auto pipe(_popen(command, "r"));
+		auto pipe(POPEN(command, "r"));
 		if (!pipe) throw make_exception("popen() failed!");
 		for (std::array<char, 128> buffer{}; fread(&buffer, 1, buffer.size() - 1, pipe) != 0u; ) {}
-		const auto rc{ _pclose(pipe) };
+		const auto rc{ PCLOSE(pipe) };
 		if (rc == -1) throw make_exception("pclose() failed!");
 		Global.return_code = std::make_unique<int>(rc);
 		return rc;
